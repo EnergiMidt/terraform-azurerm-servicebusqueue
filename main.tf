a2e7373172c5b0aefa01b6880c1c70c501dbc3c0
Leonard Sheng Sheng Lee <leonard.shengshenglee@tensio.no>
2023-06-29 13:45:28 +0200

    feature: tc-783 service bus queue

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 locals {
   name     = var.override_name == null ? "${var.system_short_name}-${var.app_name}-${lower(var.environment)}-sbq" : var.override_name
-  location = var.override_location == null ? var.resource_group.location : var.override_location
 }
 
 resource "azurerm_servicebus_queue" "servicebus_queue" {
